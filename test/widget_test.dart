cc1197555616c1b63bd8d8a52a7c7ad0c04f309e
BuddhikaBICT-UoR-FoT-6 <buddhikadarshan1475@gmail.com>
2026-02-28 11:30:00 +0530

    fix(test): remove unused flutter/material.dart import in widget_test
    Resolves flutter analyze warning: unused_import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 // the stale MyApp reference from the Flutter starter template.
 // =============================================================================
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:smart_campus/main.dart';
 
